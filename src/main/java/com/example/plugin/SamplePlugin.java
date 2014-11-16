804076a5f66391c2ad57feb3d71cadf214688732
Adam Constabaris <adam_constabaris@ncsu.edu>
2014-11-16 15:39:01 -0500

    Use Spring-defined mechanisms for loading plugins and child contexts.

--- src/main/java/com/example/plugin/SamplePlugin.java
+++ src/main/java/com/example/plugin/SamplePlugin.java
@@ -1,79 +1,68 @@
 package com.example.plugin;
 
-import edu.ncsu.lib.plugin.Plugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
- * Sample plugin showing how to load a Spring XML file and
+ * Sample plugin.  This one uses @PostConstruct and @PreDestroy annotations to invoke any startup and
+ * shutdown tasks, respectively.
  */
-public class SamplePlugin implements Plugin {
+@Component("samplePlugin")
+@Order(1)
+public class SamplePlugin {
 
     private ApplicationContext parentContext;
 
+    @Autowired
+    private DataSource dataSource;
+
     private Logger logger = LoggerFactory.getLogger(SamplePlugin.class);
-    @Override
+
     public void setParentContext(ApplicationContext parentContext) {
         this.parentContext = parentContext;
     }
 
-    @Override
-    public Map<String,? extends BeanDefinition> init() {
-        logger.trace("For lo, I am initialized.");
-       GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        ctx.setParent(parentContext);
-        Resource r = new UrlResource(getClass().getResource("pluginConfig.xml"));
-        logger.info("Searching for plugin configuration at " + r.toString());
-        if ( r.exists() ) {
-            logger.info("Reading plugin bean definitions from {}", r.toString());
-            XmlBeanDefinitionReader bReader = new XmlBeanDefinitionReader(ctx);
-            bReader.loadBeanDefinitions(r);
-        } else {
-            logger.error("Bean definition file for plugin not found");
+
+    @PostConstruct
+    public void initialize() {
+        logger.info("Initialize; dataSource {}", dataSource);
+        if ( dataSource != null ) {
+            initializeDataSource();
         }
-
-        initializeDataSource(ctx);
-
-        Map<String,BeanDefinition> defs = new HashMap();
-                for ( String bdn : ctx.getBeanDefinitionNames() ) {
-                    defs.put(bdn, ctx.getBeanDefinition(bdn));
-                }
-
-
-        return defs;
     }
 
     /**
      * Sample initialization code; shows how to pull a bean from a parent context,
      * and makes some assertions to ensure it's working as designed.
-     * @param ctx
      */
-    private void initializeDataSource(GenericXmlApplicationContext ctx) {
-        DataSource ds = (DataSource)ctx.getBean("dataSource");
-        try( Connection conn = ds.getConnection() ) {
+    private void initializeDataSource() {
+        logger.info("Initializing data source with new table");
+        try( Connection conn = dataSource.getConnection() ) {
             try (Statement st = conn.createStatement()) {
+                logger.info("Checking for table (should not exist)");
                 st.execute("SELECT * FROM plugin_table");
                 throw new IllegalStateException("Should not have been able to select from a nonexistent table");
             } catch (SQLException sqx) {
+                logger.info("Table does not exist, creating");
                 // normal and expected
             }
             try (Statement cts = conn.createStatement()) {
+                logger.info("create table");
                 cts.execute("CREATE TABLE plugin_table( id INT PRIMARY KEY, name VARCHAR(32) )");
+                logger.info("inserting sample row");
                 cts.execute("INSERT INTO plugin_table(id,name) VALUES(1,'broos wain')");
             }
             try (Statement count = conn.createStatement()) {
@@ -88,7 +77,14 @@ public class SamplePlugin implements Plugin {
         }
     }
 
+    @PreDestroy
     public void shutdown() {
+        logger.info("Shutting down");
         // nop
+    }
+
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 }
--- src/main/java/com/example/plugin/SamplePluginController.java
+++ src/main/java/com/example/plugin/SamplePluginController.java
@@ -2,25 +2,71 @@ package com.example.plugin;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Sample Controller class defined in plugin.
+ * Sample Controller class defined in plugin's <code>pluginConfig.xml</code> file.
  */
 @Controller
 public class SamplePluginController {
 
     private static final Logger logger = LoggerFactory.getLogger(SamplePluginController.class);
 
+    @Autowired
+    private DataSource dataSource;
+
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @RequestMapping(value={ "/plugin" }, method=RequestMethod.GET)
     public HttpEntity<String> getIndexPage() {
-        logger.info("HEY LOOK THE PLUGIN CONTROLLER HAS BEEN INVOKED");
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "text/plain");
-        return new HttpEntity<String>("you have been served (by a plugin)", headers);
+        StringBuilder sb = new StringBuilder();
+        for( Map<String,?> row : runQuery() ) {
+            sb.append("\nid: ").append(row.get("id") )
+                    .append("\nname: ").append(row.get("name") );
+        }
+        return new HttpEntity<String>("you have been served (by a plugin).\nHere is the data it inserted into " +
+                "its very own table:\n" + sb.toString(), headers);
+    }
+
+    private List<Map<String,Object>> runQuery() {
+        List<Map<String,Object>> results = new ArrayList<>();
+        try( Connection conn = dataSource.getConnection() ) {
+            try(Statement st = conn.createStatement() ) {
+                try( ResultSet rs = st.executeQuery("SELECT * from plugin_table") ) {
+                    while( rs.next() ) {
+                        Map<String, Object> row = new HashMap<>();
+                        row.put("id", String.valueOf(rs.getInt("id")));
+                        row.put("name", rs.getString("name"));
+                        results.add(row);
+                    }
+
+                }
+            }
+
+        } catch( SQLException sqx ) {
+            logger.warn("error accessing database", sqx);
+        }
+        return results;
+
     }
 }
